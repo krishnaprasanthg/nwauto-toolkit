acd7b9a354f572d9f47298b7bc4ba43408c6ee6d
Krishna Prasanth <krishnaprasanth.g@gmail.com>
2021-07-22 21:25:13 +0530

    update to multi-vendor for scrapli

--- cli-scripts/run-command-scrapli.py
+++ cli-scripts/run-command-scrapli.py
@@ -1,21 +1,38 @@
 import asyncio
-from scrapli.driver.core import AsyncIOSXEDriver
+from scrapli.driver.core import (
+    AsyncIOSXEDriver,
+    AsyncNXOSDriver,
+    AsyncJunosDriver,
+    AsyncIOSXRDriver,
+    AsyncEOSDriver,
+)
 import click
 import getpass
 from rich import print
+from time import time
+from math import floor
 
 from asyncio.exceptions import TimeoutError
 from scrapli.exceptions import ScrapliAuthenticationFailed
 
+DRIVER_MAP = {
+    "ios": AsyncIOSXEDriver,
+    "junos": AsyncJunosDriver,
+    "eos": AsyncEOSDriver,
+    "nxos": AsyncNXOSDriver,
+    "iosxr": AsyncIOSXRDriver,
+}
 
-async def get_command_device(device, command):
-    conn = AsyncIOSXEDriver(**device)
+
+async def get_command_device(device, command, os_type):
+
+    conn = DRIVER_MAP[os_type](**device)
     try:
         await conn.open()
     except TimeoutError:
-        return device["host"], "Connection failed"
+        return device["host"], "[red]Connection failed[/red]"
     except ScrapliAuthenticationFailed:
-        return device["host"], "Connection failed. Timeout"
+        return device["host"], "[red]Connection failed. Timeout[/red]"
 
     print(f"Connection successful to {device['host']}")
     prompt_result = await conn.get_prompt()
@@ -28,8 +45,8 @@ async def get_command_device(device, command):
         return prompt_result, "empty response"
 
 
-async def runner(DEVICES, COMMAND):
-    coroutines = [get_command_device(device, COMMAND) for device in DEVICES]
+async def runner(DEVICES, COMMAND, OS_TYPE):
+    coroutines = [get_command_device(device, COMMAND, OS_TYPE) for device in DEVICES]
     results = await asyncio.gather(*coroutines)
     for result in results:
         print("=" * 50)
@@ -43,38 +60,64 @@ async def runner(DEVICES, COMMAND):
 
 @click.command()
 @click.option(
-    "--devices",
-    "-d",
-    default="devices.txt",
+    "--device_list",
     help="simple text file with list of devices",
 )
 @click.option(
-    "--command",
-    "-c",
-    default=" show version | inc uptime ",
-    prompt="Enter command: ",
-    help="a single command in quotes",
+    "--os_type",
+    "-t",
+    default="ios",
+    help="choose from nxos, ios, junos, iosxr, eos",
 )
-def main(devices, command):
+@click.option(
+    "--device",
+    "-d",
+    multiple=True,
+    help="ip or dns of the device, can pass multiple",
+)
+@click.argument(
+    "command",
+    default="show version | inc uptime ",
+)
+def main(device_list, device, os_type, command):
 
-    # get input
-    username = input("Username: ")
-    password = getpass.getpass("Password: ")
+    devices = []
+    if device_list:
+        with open(device_list, "r") as f:
+            output = f.readlines()
+        devices = [device.strip() for device in output]
+    else:
+        if device:
+            devices.extend(list(device))
 
-    with open(devices, "r") as f:
-        output = f.readlines()
-    devices = [device.strip() for device in output]
-    devices_dict = [
-        {
-            "host": device,
-            "auth_username": f"{username}",
-            "auth_password": f"{password}",
-            "auth_strict_key": False,
-            "transport": "asyncssh",
-        }
-        for device in devices
-    ]
-    asyncio.run(runner(devices_dict, command))
+    if len(devices):
+
+        # get credentials
+        username = input("Username: ")
+        password = getpass.getpass("Password: ")
+
+        devices_dict = [
+            {
+                "host": device,
+                "auth_username": f"{username}",
+                "auth_password": f"{password}",
+                "auth_strict_key": False,
+                "transport": "asyncssh",
+            }
+            for device in devices
+        ]
+        start_time = time()
+        asyncio.run(runner(devices_dict, command, os_type))
+
+        end_time = time()
+        time_taken = (end_time - start_time) / 60
+        minutes = floor(time_taken)
+        seconds = floor((time_taken % 1) * 60)
+        print(
+            f"[green]Time taken:[/green] [blue]{minutes:02d}:{seconds:02d} minutes[/blue]"
+        )
+    else:
+        print("no device inputs passed!!! ")
 
 
 if __name__ == "__main__":

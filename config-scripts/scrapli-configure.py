a98e07609312322aa3297b2e15de7bf4fa7c013b
Krishna Prasanth <krishnaprasanth.g@gmail.com>
2021-08-26 22:19:11 +0530

    change ssh library to ssh2 fix #2

--- config-scripts/scrapli-configure.py
+++ config-scripts/scrapli-configure.py
@@ -1,121 +1,95 @@
-import asyncio
-from scrapli.driver.core import (
-    AsyncIOSXEDriver,
-    AsyncNXOSDriver,
-    AsyncJunosDriver,
-    AsyncIOSXRDriver,
-    AsyncEOSDriver,
-)
-import click
+import concurrent.futures
 import getpass
-from rich import print
-from time import time
+import sys
 from math import floor
 from os.path import isfile
+from time import time
 
-from asyncio.exceptions import TimeoutError
-from scrapli.exceptions import ScrapliAuthenticationFailed, ScrapliTimeout
+import click
+from rich import print
+from scrapli.driver.core import (
+    EOSDriver,
+    IOSXEDriver,
+    IOSXRDriver,
+    JunosDriver,
+    NXOSDriver,
+)
 
 DRIVER_MAP = {
-    "ios": AsyncIOSXEDriver,
-    "junos": AsyncJunosDriver,
-    "eos": AsyncEOSDriver,
-    "nxos": AsyncNXOSDriver,
-    "iosxr": AsyncIOSXRDriver,
+    "ios": IOSXEDriver,
+    "eos": EOSDriver,
+    "nxos": NXOSDriver,
+    "iosxr": IOSXRDriver,
+    "junos": JunosDriver,
+}
+SAVE_COMMAND = {
+    "ios": "copy running-config startup-config",
+    "eos": "copy running-config startup-config",
+    "nxos": "copy running-config startup-config",
+    "iosxr": "copy running-config startup-config",
+    "junos": "commit",
 }
 
 
-async def configure_device(device, cfg_file, os_type):
+def device_handler(device, os_type, config_list):
+    success = False
+    print(f"handling device {device['host']}")
+    config_list.append("!")
 
-    conn = DRIVER_MAP[os_type](**device)
     try:
-        await conn.open()
-    except TimeoutError:
-        return device["host"], "[red]Connection failed[/red]"
-    except ScrapliAuthenticationFailed:
-        return device["host"], "[red]Authentication failed. Check the credentials[/red]"
-    except ScrapliTimeout:
-        return (
-            device["host"],
-            "[red]Connection failed. Timeout. Verify if the OS_TYPE argument passed. [/red]",
-        )
-
-    print(f"Connection successful to {device['host']}")
-    prompt_result = await conn.get_prompt()
-    output = await conn.send_configs_from_file(file=cfg_file, stop_on_failed=True)
-    if output.failed == True:
-        print(
-            f"[red]Failed to configure the device {device['host']}. Skipping write[/red]"
-        )
-    else:
-        print(f"Configured the device {device['host']}")
-        save_result = await conn.send_command(f"copy running-config startup-config")
-        print(
-            f"[yellow]Saving configuration for device: {device['host']} {save_result.result}[/yellow]"
-        )
-    await conn.close()
-    if len(output.result):
-        return prompt_result, output
-    else:
-        return prompt_result, "empty response"
-
-
-async def runner(DEVICES, CFG_FILE, OS_TYPE):
-    coroutines = [configure_device(device, CFG_FILE, OS_TYPE) for device in DEVICES]
-    results = await asyncio.gather(*coroutines)
-    for result in results:
-        print("=" * 50)
-        print(f"[green]>>>> device: [bold]{result[0]}[/bold][/green]")
-        print("=" * 50)
-        results = [
-            res.channel_input
-            if not res.failed
-            else "[red]"
-            + str(res.channel_input)
-            + "\n"
-            + str(res.raw_result)
-            + "[/red]"
-            for res in result[1]
-        ]
-        for res in results:
-            print(res)
-        print("-" * 50)
+        with DRIVER_MAP[os_type](**device) as conn:
+            print(f"connected to device {device['host']}")
+            response = conn.send_configs(configs=config_list, stop_on_failed=True)
+            if not len(response) == len(config_list):
+                success = False
+                result = response[-1].channel_input + " - " + response[-1].result
+            else:
+                success = True
+                # save config per device type
+                conn.send_command(str(SAVE_COMMAND[os_type]))
+                print(f"Saved configuration to {device['host']}")
+                result = "Success"
+    except Exception as e:
+        result = e
+        print(f"Error - {e}")
+    return (device, success, result)
 
 
 @click.command()
-@click.option(
-    "--device_list",
-    help=".txt file with list of devices",
-)
-@click.option(
-    "--config_file",
-    help=".txt file with list of configuration commands",
-)
-@click.option(
-    "--os_type",
-    "-t",
-    default="ios",
-    help="choose from nxos, ios, junos, iosxr, eos. Default is ios",
-)
 @click.option(
     "--device",
     "-d",
     multiple=True,
     help="ip or dns of the device, chain it for multiple devices ",
 )
-def main(device_list, device, os_type, config_file):
+def main(device):
+    config_file = click.prompt("Provide the configuration file", default="config.txt")
+    devices = []
+    if device:
+        devices.extend(list(device))
+    else:
+        device_list = click.prompt(
+            "Provide the file with list of devices", default="devices.txt"
+        )
+        if not isfile(path=device_list):
+            print("provide the list of devices. See HELP.. ")
+            sys.exit()
+        with open(device_list, "r") as f:
+            output = f.readlines()
+            devices = [device.strip() for device in output]
+    num_workers = click.prompt("Enter the number of workers", default=40)
+    os_type = click.prompt(
+        "Provide the device OS", type=click.Choice(DRIVER_MAP.keys()), default="ios"
+    )
 
     if not isfile(path=config_file):
         print("provide the config file. See HELP.. ")
-        exit()
-    devices = []
-    if device_list:
-        with open(device_list, "r") as f:
-            output = f.readlines()
-        devices = [device.strip() for device in output]
-    else:
-        if device:
-            devices.extend(list(device))
+        sys.exit()
+
+    config = []
+    with open(config_file, "r") as f:
+        output = f.readlines()
+        config = [config_line.strip() for config_line in output]
 
     if len(devices):
 
@@ -129,13 +103,52 @@ def main(device_list, device, os_type, config_file):
                 "auth_username": f"{username}",
                 "auth_password": f"{password}",
                 "auth_strict_key": False,
-                "transport": "asyncssh",
+                "transport": "ssh2",
             }
             for device in devices
         ]
         start_time = time()
-        asyncio.run(runner(devices_dict, config_file, os_type))
 
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+            results = []
+            processed = 0
+            total = len(devices)
+
+            print(f"Executing against {total} devices with {num_workers} workers.")
+
+            futures = {
+                executor.submit(device_handler, device, os_type, config)
+                for device in devices_dict
+            }
+
+            # Wait for the futures to complete and give status updates
+            try:
+                for future in concurrent.futures.as_completed(futures):
+                    processed += 1
+                    percent = round((processed / total) * 100)
+                    print(f"{processed} of {total} ({percent}%) devices processed.")
+            except (SystemExit, KeyboardInterrupt) as e:
+                if isinstance(e, KeyboardInterrupt):
+                    print("===============================================")
+                    print("         Keyboard interrupt detected           ")
+                    print("                                               ")
+                    print("        Cancelling remaining devices           ")
+                    print("  Currently executing devices remain running   ")
+                    print("===============================================")
+                # Cancel remaining work
+                for f in futures:
+                    f.cancel()
+                executor.shutdown(wait=False)
+                # System exits will be raised again so the code calling this function
+                # also exits
+                if isinstance(e, SystemExit):
+                    raise
+            print("Collecting results")
+            for f in futures:
+                try:
+                    results.append(f.result())
+                except concurrent.futures.CancelledError:
+                    print("Skipping cancelled device")
         end_time = time()
         time_taken = (end_time - start_time) / 60
         minutes = floor(time_taken)
@@ -143,6 +156,9 @@ def main(device_list, device, os_type, config_file):
         print(
             f"[green]Time taken:[/green] [blue]{minutes:02d}:{seconds:02d} minutes[/blue]"
         )
+        for result in results:
+            device, success, result = result
+            print(f"{device['host']} --> {success} -> {result}")
     else:
         print("no device inputs passed!!! ")
 
